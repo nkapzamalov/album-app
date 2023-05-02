3f6536d22c43944730fcaff1264e6c3d3cd0cad9
nkapzamalov <115000079+nkapzamalov@users.noreply.github.com>
2023-05-02 19:48:03 +0300

    wip

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <div className="flex justify-around bg-white py-4 px-8 drop-shadow-lg mb-8">
-      <Link to={'/'} className="text-black text-xl font-semibold">
+      <Link to='/' className="text-black text-xl font-semibold">
         Albums
       </Link>
-      <Link to={'/favorites'} className="text-black text-xl font-semibold">
+      <Link to='/favorites' className="text-black text-xl font-semibold">
         Favorites
       </Link>
     </div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,14 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import favoritesReducer from './favoritesSlice';
@@ -17,6 +26,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 const persistor = persistStore(store);
